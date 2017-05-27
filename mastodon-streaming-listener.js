0fac347d102a33b0f4380715f10f2f0f0d8f550c
tateisu <tateisu@gmail.com>
2017-05-28 08:52:28 +0900

    WebSocketの再接続待機や接続処理中にregister や unregister が呼び出された場合の挙動を改善する

--- mastodon-streaming-listener.js
+++ mastodon-streaming-listener.js
@@ -389,8 +389,6 @@ const ListenerConnection = function (log, ws_key, registration) {
 ListenerConnection.prototype.clearTimers = function () {
     const self = this;
 
-    npmlog.log('info',"debug","clearTimers "+ util.inspect(self))
-    
     if (self.heartbeat) {
         clearInterval(self.heartbeat)
         self.heartbeat = null
